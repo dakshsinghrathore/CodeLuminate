e0d26445eb5e113ba04ed8dda5a03187b09137e6
dakshsinghrathore <dakshrathore1729@gmail.com>
2024-03-23 00:50:57 +0530

    code: added githubAPI key

--- src/utils/githubAPI.ts
+++ src/utils/githubAPI.ts
@@ -1,9 +1,18 @@
 export async function getUserData(username: string) {
-    const response = await fetch(`https://api.github.com/users/${username}`);
+  const accessToken = "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";
+  try {
+    const response = await fetch(`https://api.github.com/users/${username}`, {
+      headers: {
+        Authorization: `token ${accessToken}`
+      }
+    });
     if (!response.ok) {
-      throw new Error("Failed to fetch user data");
+      throw new Error(`Failed to fetch user data: ${response.statusText}`);
     }
     const data = await response.json();
     return data;
+  } catch (error) {
+    console.error("Error fetching user data:", error);
+    throw error; 
   }
-  
+}
